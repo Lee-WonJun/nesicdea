61a86c8f983af8c22e17385f250a76fb51443cd6
WonJun Lee <dldnjs1013@nate.com>
2025-04-10 01:31:32 +0900

    날짜 선택 UI 개선 및 CSV 생성 버튼 추가. 시작 및 종료 날짜 선택을 위한 모달을 조건부로 렌더링하도록 수정하였으며, 버튼 스타일을 개선하고 취소 버튼을 추가했습니다.

--- src/nesicdea/views/export.cljs
+++ src/nesicdea/views/export.cljs
@@ -26,25 +26,29 @@
                   :theme {:todayTextColor (:primary s/colors)
                           :selectedDayBackgroundColor (:primary s/colors)}}]
      [c/touchable-opacity {:style {:align-items "center"
-                                   :padding 10
-                                   :margin-top 10}
+                                    :padding 10
+                                    :margin-top 10}
                            :on-press on-close}
       [c/text {:style {:color (:primary s/colors)
                        :font-weight "bold"}}
-       "닫기"]]]]])
+       "닫기"]]]]]) ; <-- Closing brackets for date-picker-modal seem correct
 
 (defn export-view []
   (let [show-start-picker (r/atom false)
         show-end-picker (r/atom false)
-        start-date (r/atom (doto (js/Date.) (.setDate (.getDate (js/Date.)) - 30)))
-        end-date (r/atom (js/Date.))]
+        start-date (r/atom (doto (js/Date.) (.setDate (.getDate (js/Date.)) - 30))) ; Default start date: 30 days ago
+        end-date (r/atom (js/Date.))] ; Default end date: today
     (fn [] ; Return render function
       [c/view {:style (merge (:container s/common-styles)
                              {:align-items "center"
                               :justify-content "center"})}
+       ; Title
        [c/text {:style {:font-size 20 :color "#333" :margin-bottom 20}}
         "날짜 범위 선택"]
+
+       ; Date Selection Area
        [c/view {:style {:width "80%" :margin-bottom 20}}
+        ; Start Date Display and Trigger
         [c/text {:style {:font-size 16 :margin-bottom 10 :align-self "flex-start"}} "시작 날짜:"]
         [c/touchable-opacity {:on-press #(reset! show-start-picker true)}
          [c/view {:style {:flex-direction "row"
@@ -58,8 +62,9 @@
                           :margin-bottom 15}}
           [c/text {:style {:font-size 16 :color "#555"}}
            (.toLocaleDateString @start-date)]
-          [c/text {:style {:color (:primary s/colors)}} "변경"]]]
+          [c/text {:style {:color (:primary s/colors)}} "변경"]]] ; <-- Close inner view and touchable opacity for start date
 
+        ; End Date Display and Trigger
         [c/text {:style {:font-size 16 :margin-bottom 10 :align-self "flex-start"}} "종료 날짜:"]
         [c/touchable-opacity {:on-press #(reset! show-end-picker true)}
          [c/view {:style {:flex-direction "row"
@@ -73,30 +78,35 @@
                           :margin-bottom 15}}
           [c/text {:style {:font-size 16 :color "#555"}}
            (.toLocaleDateString @end-date)]
-          [c/text {:style {:color (:primary s/colors)}} "변경"]]]]]
+          [c/text {:style {:color (:primary s/colors)}} "변경"]]]] 
 
-       ;; Start Date Picker Modal
-      (when @show-start-picker
-        (date-picker-modal @show-start-picker start-date show-start-picker #(reset! show-start-picker false)))
+       ; Start Date Picker Modal (Conditionally Rendered)
+       (when @show-start-picker
+         (date-picker-modal @show-start-picker start-date show-start-picker #(reset! show-start-picker false)))
 
-       ;; End Date Picker Modal
-      (when @show-end-picker
-        (date-picker-modal @show-end-picker end-date show-end-picker #(reset! show-end-picker false)))
+       ; End Date Picker Modal (Conditionally Rendered)
+       (when @show-end-picker
+         (date-picker-modal @show-end-picker end-date show-end-picker #(reset! show-end-picker false)))
 
-      [c/touchable-highlight
-       {:style (merge (:button s/common-styles)
-                      {:background-color (:secondary s/colors)
-                       :width "80%"
-                       :margin-bottom 10})
-        :on-press #(do
-                     (rf/dispatch [:set-export-dates @start-date @end-date])
-                     ;; TODO: Implement CSV generation logic
-                     (js/console.log "CSV 생성하기 로직 실행 예정")
-                     (rf/dispatch [:generate-csv]))}
-       [c/text {:style (:buttonText s/common-styles)} "CSV 생성하기"]]
-      [c/touchable-highlight
-       {:style (merge (:button s/common-styles)
-                      {:background-color "#b00020"
-                       :width "80%"})
-        :on-press #(rf/dispatch [:set-view :main])}
-       [c/text {:style (:buttonText s/common-styles)} "취소"]])))
+       ; Export Button
+       [c/touchable-highlight
+        {:style (merge (:button s/common-styles)
+                       {:background-color (:secondary s/colors)
+                        :width "80%"
+                        :margin-bottom 10})
+         :on-press #(do
+                      (rf/dispatch [:set-export-dates @start-date @end-date])
+                      ;; TODO: Implement CSV generation logic
+                      (js/console.log "CSV 생성하기 로직 실행 예정")
+                      (rf/dispatch [:generate-csv]))}
+        [c/text {:style (:buttonText s/common-styles)} "CSV 생성하기"]]
+
+       ; Cancel Button
+       [c/touchable-highlight
+        {:style (merge (:button s/common-styles)
+                       {:background-color "#b00020" ; Consider using a color from s/colors if available
+                        :width "80%"})
+         :on-press #(rf/dispatch [:set-view :main])}
+        [c/text {:style (:buttonText s/common-styles)} "취소"]]]
+
+      ))) 
